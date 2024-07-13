2a5ea92c1db3256eb326eabcc171b4874579b234
Anano Topuria <anano.topuriaa@gmail.com>
2024-07-13 22:40:36 +0400

    feat: add routing configuration with createBrowserRouter

---
+++ src/components/Layout/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+const Header = () => {
+  return (
+    <header>
+      <p>this is header</p>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/routes.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/Layout";
+import HomePage from "./homePage";
+import Product from "./product";
+import Products from "./products";
+
+const router = createBrowserRouter([
+  {
+    path: "/starter_project",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "product",
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
+export default router;
